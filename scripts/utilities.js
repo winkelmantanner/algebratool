155d143ed421edb3d1ce1d7b500ea488d154670b
Tanner Winkelman <21373626+winkelmantanner@users.noreply.github.com>
2019-06-26 11:09:00 -0500

    Match more stuff.  Def not prod ready.

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -464,3 +464,25 @@ function hash_node(node) {
   }
 }
 
+
+// https://stackoverflow.com/questions/9960908/permutations-in-javascript
+function permutator(inputArr) {
+  var results = [];
+
+  function permute(arr, memo) {
+    var cur, memo = memo || [];
+
+    for (var i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+
+    return results;
+  }
+
+  return permute(inputArr);
+}
